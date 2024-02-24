6a6be0041a6460194aabbe7eb3f2fea9e20e260c
Shamna K <shamnashami500@gmail.com>
2024-02-24 23:26:17 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Counter from './Counter';
+import {useState} from 'react';
 function App() {
-  return (
+  const [state, setState]=useState(false);
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 onClick={()=>setState(!state)}>Show/Hide</h1>
+      { state? <Counter /> : null }
+    </div> 
   );
 }
 
 export default App;
+
